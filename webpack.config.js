2420b9d545f737bb9627e3dab5d4bed57ec93bc2
Taher <ttaherhagui@gmail.com>
2021-11-09 13:37:30 +0100

    set up linter checks.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -26,7 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Leaderboard',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
 };
