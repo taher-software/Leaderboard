200eab8cdf7e1a13ff4ea95eb741ff1905beb9fa
Taher <ttaherhagui@gmail.com>
2021-11-10 20:52:56 +0100

    Add fetaures which allow to consume API.

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,43 @@
-const addScore = () => {
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const addNewGame = (endpoint = '', gameName = '') => {
+  const targetUrl = url + endpoint;
+  const data = { name: gameName };
+  return fetch(targetUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((resp) => resp.json());
+};
+
+const getScore = (id = '') => {
+  const targetUrl = `${url}games/${id}/scores/`;
+  return fetch(targetUrl).then((resp) => resp.json());
+};
+
+const addScore = (user, score) => {
   const newSCore = document.createElement('div');
   const playersScores = document.querySelector('.players-scores');
   const newScoreOrder = playersScores.children.length + 1;
-  const [playerName, playerScore] = Array.from(document.querySelectorAll('.user-input'));
-  newSCore.innerHTML = `<p>${playerName.value}: ${playerScore.value}`;
+  newSCore.innerHTML = `<p>${user}: ${score}`;
   newSCore.className = 'score';
   if (newScoreOrder % 2 === 0) newSCore.style.backgroundColor = 'rgb(221,221,221)';
   else newSCore.style.backgroundColor = '#fff';
   playersScores.appendChild(newSCore);
 };
-export default addScore;
+const postScore = (userName, userScore, id) => {
+  const targetUrl = `${url}games/${id}/scores`;
+  const data = { user: userName, score: userScore };
+  return fetch(targetUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((resp) => resp.json());
+};
+export {
+  url, addNewGame, getScore, addScore, postScore,
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import './style.css';
-import addScore from './functions';
+import {
+  addNewGame, getScore, addScore, postScore,
+} from './functions';
 
-const btn = document.querySelector('.submit-button');
-btn.addEventListener('click', addScore);
+async function consumeAPI() {
+  const gameData = await addNewGame('games/', 'java game');
+  const id = gameData.result.split(':')[1].trim().split(' ')[0];
+  const refresh = document.querySelector('.refresh');
+  const submitBtn = document.querySelector('.submit-button');
+  refresh.addEventListener('click', () => {
+    async function userScores() {
+      const resultScore = await getScore(id);
+      const playersScore = resultScore.result;
+      playersScore.forEach((element) => {
+        addScore(element.user, element.score);
+      });
+    }
+    userScores();
+  });
+  submitBtn.addEventListener('click', () => {
+    const [userName, userScore] = Array.from(document.querySelectorAll('.user-input'));
+    async function saveScore() {
+      await postScore(userName.value, userScore.value, id);
+    }
+    saveScore();
+  });
+}
+consumeAPI();
